8bc2c477aee5d39041357f131304fba66eb85876
Neide Urbina <neide.n.urbina@gmail.com>
2022-02-02 22:26:50 -0600

    messed up commit, adding rest of the front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+//Import Components 
+import Header from './components/header';
+import Footer from './components/footer';
+import { Route, Switch } from "react-router-dom";
+
+//Import Pages
+import Home from './pages/home';
+import About from './pages/about';
+import Projects from './pages/projects';
+
 function App() {
+  const URL = "https://n-urbina-portfolio.herokuapp.com/";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route path="/projects">
+            <Projects URL={URL} />
+        </Route>
+        <Route path="/about">
+            <About URL={URL} />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
